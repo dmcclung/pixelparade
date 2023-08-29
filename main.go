16f71788efdcdb1a733831b3325536b7c06f8a41
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-08-29 11:03:50 -0500

    Added not found handler

--- main.go
+++ main.go
@@ -5,6 +5,23 @@ import (
 	"net/http"
 )
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+  switch r.URL.Path {
+  case "/contact": 
+    contactHandler(w, r)
+  case "/":
+    homeHandler(w, r)
+  default: 
+    notFoundHandler(w, r)
+  }
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusNotFound)
+  w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  fmt.Fprint(w, "<h1>Not found</h1><p>Sorry what you requested was not found</p>")
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
@@ -16,8 +33,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-  http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
