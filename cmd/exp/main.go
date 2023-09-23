4d8e070b886dc387d23f6a0e275e6ea43bff1ab4
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-09-23 00:10:22 -0500

    Implementing email service

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -3,22 +3,32 @@ package main
 import (
 	"os"
 
+	"github.com/dmcclung/pixelparade/models"
 	"github.com/go-mail/mail/v2"
 )
 
 func main() {
 	from := "dylan@pixelparade"
-	to := "auser@gmail.com"
+	to := "guest@pixelparade"
 	subject := "This is a test email"
 	plaintext := "This is the body of the email"
 	html := `<h1>Hello there buddy!</h1><p>This is the email</p><p>Hope you enjoy it</p>`
 
-	msg := mail.NewMessage()
-	msg.SetHeader("To", to)
-	msg.SetHeader("From", from)
-	msg.SetHeader("Subject", subject)
-	msg.SetBody("text/plain", plaintext)
-	msg.AddAlternative("text/html", html)
-	msg.WriteTo(os.Stdout)
+	m := mail.NewMessage()
+	m.SetHeader("To", to)
+	m.SetHeader("From", from)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/plain", plaintext)
+	m.AddAlternative("text/html", html)
+	m.WriteTo(os.Stdout)
 
+	emailService, err := models.GetEmailService()
+	if err != nil {
+		panic(err)
+	}
+
+	err = emailService.SendEmail(m)
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ models/email.go
@@ -0,0 +1,74 @@
+package models
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/go-mail/mail/v2"
+	"github.com/joho/godotenv"
+)
+
+const (
+	DefaultSender = "support@pixelparade"
+)
+
+type SMTPConfig struct {
+	Host string
+	Port int
+	User string
+	Pass string
+}
+
+func GetEmailConfig() (*SMTPConfig, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("loading env: %w", err)
+	}
+
+	envVars := []string{"EMAIL_HOST", "EMAIL_PORT", "EMAIL_USER", "EMAIL_PASS"}
+	args := []string{}
+	for _, envVar := range envVars {
+		val, exists := os.LookupEnv(envVar)
+		if !exists {
+			return nil, fmt.Errorf("%v not found", envVar)
+		}
+		args = append(args, val)
+	}
+
+	p, err := strconv.Atoi(args[1])
+	if err != nil {
+		return nil, fmt.Errorf("converting port: %w", err)
+	}
+
+	config := SMTPConfig{
+		Host: args[0],
+		Port: p,
+		User: args[2],
+		Pass: args[3],
+	}
+
+	return &config, nil
+}
+
+type EmailService struct {
+	dialer *mail.Dialer
+}
+
+func GetEmailService() (*EmailService, error) {
+	config, err := GetEmailConfig()
+	if err != nil {
+		return nil, fmt.Errorf("email config: %w", err)
+	}
+
+	dialer := mail.NewDialer(config.Host, config.Port, config.User, config.Pass)
+
+	return &EmailService{
+		dialer: dialer,
+	}, nil
+}
+
+func (es *EmailService) SendEmail(m *mail.Message) error {
+	err := es.dialer.DialAndSend(m)
+	return err
+}
