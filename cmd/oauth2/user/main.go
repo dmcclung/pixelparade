76da915074d6f8bf39c6e5cb41e96e84b67df914
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-11-04 22:45:26 -0500

    cmd for id token validation

---
+++ cmd/oauth2/user/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/dmcclung/pixelparade/jwt"
+)
+
+func main() {
+	fmt.Printf("Paste ID token here and press enter: ")
+	
+	var idToken string
+	if _, err := fmt.Scan(&idToken); err != nil {
+		log.Fatal(err)
+		return
+	}
+	
+	subject, err := jwt.GetSubFromJWT(idToken)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	log.Printf("User ID: %v\n", subject)
+}
