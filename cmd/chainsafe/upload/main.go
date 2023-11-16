fd3af757f88cd299ea1ef0a7adbb953d12adfa4b
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-11-16 15:42:18 -0600

    added image upload to chainsafe filecoin

--- cmd/chainsafe/upload/main.go
+++ cmd/chainsafe/upload/main.go
@@ -13,42 +13,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var apiURL, apiKey string
-
-func createBucket() {
-	bucketName := "images"
-
-	jsonData := []byte(fmt.Sprintf(`{
-		"type": "fps",
-		"name": "%s"
-	}`, bucketName))
-
-	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(jsonData))
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	req.Header.Set("Authorization", "Bearer "+apiKey)
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	log.Println("Response:", string(body))
-}
-
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -56,12 +20,20 @@ func main() {
 		return
 	}
 
-	apiURL = os.Getenv("CHAINSAFE_API_URL")
-	apiKey = os.Getenv("CHAINSAFE_API_KEY")
-	
-	createBucket()
+	apiURL := os.Getenv("CHAINSAFE_API_URL")
+	apiKey := os.Getenv("CHAINSAFE_API_KEY")
+	bucketID := os.Getenv("CHAINSAFE_IMAGES_BUCKET_ID")
 
-	filePath := "/Desktop/image.png" // Replace with the path to your file
+	const galleryID = "f61dc186-c1c1-48b4-b65d-6656101bd04f"
+	const imageID = "4c7cda78-4bb1-44bd-a246-d16c850a886e"
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	filePath := fmt.Sprintf("%s/images/gallery-%s/%s.jpg", wd, galleryID, imageID)
 
 	// Create a buffer to write our multipart form data
 	body := &bytes.Buffer{}
@@ -70,35 +42,41 @@ func main() {
 	// Add the file field
 	file, err := os.Open(filePath)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 	defer file.Close()
 
 	part, err := writer.CreateFormFile("file", filepath.Base(file.Name()))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 	_, err = io.Copy(part, file)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 
 	// Add the path field
 	err = writer.WriteField("path", "/")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 
 	// Close the writer before making the request
 	err = writer.Close()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 
 	// Create the request
-	req, err := http.NewRequest("POST", apiURL, body)
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/bucket/%s/upload", apiURL, bucketID), body)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 
 	// Set the content type, this must be done after writer.Close()
@@ -109,15 +87,17 @@ func main() {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 	defer resp.Body.Close()
 
 	// Read the response
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 
-	fmt.Println("Response:", string(respBody))
+	log.Println("Response:", string(respBody))
 }
